81c07410f498c344bbaae9949a5900dddbb0b1b7
Hara prasad mahapatra <haraprasadmahapatra223@gmail.com>
2026-07-01 20:24:21 +0530

    cors issue resolved

--- backend/app.js
+++ backend/app.js
@@ -8,21 +8,23 @@ const recruitmentRoutes = require("./routes/recruitmentRoutes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "https://geeksforgeeks-six.vercel.app",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
   })
 );
+
+app.use(express.json());
+
+app.use("/recruitment", recruitmentRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
