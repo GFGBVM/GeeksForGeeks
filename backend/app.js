318b51c37a2fcfc1f7f5f481777698a4e711e134
Hara prasad mahapatra <haraprasadmahapatra223@gmail.com>
2026-07-01 19:41:24 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/recruitment", recruitmentRoutes);
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://geeksforgeeks-six.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
